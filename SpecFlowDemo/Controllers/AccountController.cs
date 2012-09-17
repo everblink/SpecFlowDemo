c5a157839d4ce604e02d64e4ca12cf389416d9ad
Jeffrey Ng <jeffrey.s.h.ng@gmail.com>
2012-09-17 14:45:18 +0100

    Fixed one test to redirect correctly
    Amending the AccountController and AccountModel so that the redirection
    will work correctly.

--- SpecFlowDemo/Controllers/AccountController.cs
+++ SpecFlowDemo/Controllers/AccountController.cs
@@ -9,11 +9,25 @@ using SpecFlowDemo.Models;
 
 namespace SpecFlowDemo.Controllers
 {
+    [HandleError]
     public class AccountController : Controller
     {
 
         public IFormsAuthenticationService FormsService { get; set; }
         public IMembershipService MembershipService { get; set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (FormsService == null) { FormsService = new FormsAuthenticationService(); }
+            if (MembershipService == null) { MembershipService = new AccountMembershipService(); }
+
+            base.Initialize(requestContext);
+        }
+
+        public AccountController()
+        {
+        }
+
         //
         // GET: /Account/LogOn
         public AccountController(IFormsAuthenticationService formsService, IMembershipService memberService)
@@ -35,11 +49,10 @@ namespace SpecFlowDemo.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (Membership.ValidateUser(model.UserName, model.Password))
+                if (MembershipService.ValidateUser(model.UserName, model.Password))
                 {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    FormsService.SignIn(model.UserName, model.RememberMe);
+                    if (!String.IsNullOrEmpty(returnUrl))
                     {
                         return Redirect(returnUrl);
                     }
@@ -63,13 +76,10 @@ namespace SpecFlowDemo.Controllers
 
         public ActionResult LogOff()
         {
-            FormsAuthentication.SignOut();
+            FormsService.SignOut();
 
             return RedirectToAction("Index", "Home");
         }
-
-        //
-        // GET: /Account/Register
 
         public ActionResult Register()
         {
@@ -78,44 +88,47 @@ namespace SpecFlowDemo.Controllers
             return View();
         }
 
-        //
-        // POST: /Account/Register
-
         [HttpPost]
         public ActionResult Register(RegisterModel model)
         {
+            //validate input
+            if (string.IsNullOrEmpty(model.UserName))
+            {
+                ModelState.AddModelError("username", "Username is required");
+            }
+
+            if (model.Password != model.ConfirmPassword)
+            {
+                ModelState.AddModelError("password", "The password and confirmation password do not match");
+            }
+
             if (ModelState.IsValid)
             {
                 // Attempt to register the user
-                MembershipCreateStatus createStatus;
-                Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
+                MembershipCreateStatus createStatus = MembershipService.CreateUser(model.UserName, model.Password, model.Email);
 
                 if (createStatus == MembershipCreateStatus.Success)
                 {
-                    FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
+                    FormsService.SignIn(model.UserName, false /* createPersistentCookie */);
                     return RedirectToAction("Index", "Home");
                 }
                 else
                 {
-                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
+                    ModelState.AddModelError("", AccountValidation.ErrorCodeToString(createStatus));
                 }
             }
 
             // If we got this far, something failed, redisplay form
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
             return View(model);
         }
-
-        //
-        // GET: /Account/ChangePassword
 
         [Authorize]
         public ActionResult ChangePassword()
         {
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
             return View();
         }
-
-        //
-        // POST: /Account/ChangePassword
 
         [Authorize]
         [HttpPost]
@@ -123,21 +136,7 @@ namespace SpecFlowDemo.Controllers
         {
             if (ModelState.IsValid)
             {
-
-                // ChangePassword will throw an exception rather
-                // than return false in certain failure scenarios.
-                bool changePasswordSucceeded;
-                try
-                {
-                    MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
-                    changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
-                }
-                catch (Exception)
-                {
-                    changePasswordSucceeded = false;
-                }
-
-                if (changePasswordSucceeded)
+                if (MembershipService.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword))
                 {
                     return RedirectToAction("ChangePasswordSuccess");
                 }
@@ -148,6 +147,7 @@ namespace SpecFlowDemo.Controllers
             }
 
             // If we got this far, something failed, redisplay form
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
             return View(model);
         }
 
@@ -158,45 +158,5 @@ namespace SpecFlowDemo.Controllers
         {
             return View();
         }
-
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
-        }
-        #endregion
     }
 }
--- SpecFlowDemo/Models/AccountModels.cs
+++ SpecFlowDemo/Models/AccountModels.cs
@@ -83,77 +83,138 @@ namespace SpecFlowDemo.Models
         bool ChangePassword(string userName, string oldPassword, string newPassword);
     }
 
-    //public class AccountMembershipService : IMembershipService
-    //{
-    //    private readonly MembershipProvider _provider;
+    public class AccountMembershipService : IMembershipService
+    {
+        private readonly MembershipProvider _provider;
 
-    //    public AccountMembershipService()
-    //        : this(null)
-    //    {
-    //    }
+        public AccountMembershipService()
+            : this(null)
+        {
+        }
 
-    //    public AccountMembershipService(MembershipProvider provider)
-    //    {
-    //        _provider = provider ?? Membership.Provider;
-    //    }
+        public AccountMembershipService(MembershipProvider provider)
+        {
+            _provider = provider ?? Membership.Provider;
+        }
 
-    //    public int MinPasswordLength
-    //    {
-    //        get
-    //        {
-    //            return _provider.MinRequiredPasswordLength;
-    //        }
-    //    }
+        public int MinPasswordLength
+        {
+            get
+            {
+                return _provider.MinRequiredPasswordLength;
+            }
+        }
 
-    //    public bool ValidateUser(string userName, string password)
-    //    {
-    //        if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
-    //        if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
+        public bool ValidateUser(string userName, string password)
+        {
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
 
-    //        return _provider.ValidateUser(userName, password);
-    //    }
+            return _provider.ValidateUser(userName, password);
+        }
 
-    //    public MembershipCreateStatus CreateUser(string userName, string password, string email)
-    //    {
-    //        if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
-    //        if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
-    //        if (String.IsNullOrEmpty(email)) throw new ArgumentException("Value cannot be null or empty.", "email");
+        public MembershipCreateStatus CreateUser(string userName, string password, string email)
+        {
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
+            if (String.IsNullOrEmpty(email)) throw new ArgumentException("Value cannot be null or empty.", "email");
 
-    //        MembershipCreateStatus status;
-    //        _provider.CreateUser(userName, password, email, null, null, true, null, out status);
-    //        return status;
-    //    }
+            MembershipCreateStatus status;
+            _provider.CreateUser(userName, password, email, null, null, true, null, out status);
+            return status;
+        }
 
-    //    public bool ChangePassword(string userName, string oldPassword, string newPassword)
-    //    {
-    //        if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
-    //        if (String.IsNullOrEmpty(oldPassword)) throw new ArgumentException("Value cannot be null or empty.", "oldPassword");
-    //        if (String.IsNullOrEmpty(newPassword)) throw new ArgumentException("Value cannot be null or empty.", "newPassword");
+        public bool ChangePassword(string userName, string oldPassword, string newPassword)
+        {
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(oldPassword)) throw new ArgumentException("Value cannot be null or empty.", "oldPassword");
+            if (String.IsNullOrEmpty(newPassword)) throw new ArgumentException("Value cannot be null or empty.", "newPassword");
 
-    //        // The underlying ChangePassword() will throw an exception rather
-    //        // than return false in certain failure scenarios.
-    //        try
-    //        {
-    //            MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
-    //            return currentUser.ChangePassword(oldPassword, newPassword);
-    //        }
-    //        catch (ArgumentException)
-    //        {
-    //            return false;
-    //        }
-    //        catch (MembershipPasswordException)
-    //        {
-    //            return false;
-    //        }
-    //    }
-    //}
+            // The underlying ChangePassword() will throw an exception rather
+            // than return false in certain failure scenarios.
+            try
+            {
+                MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
+                return currentUser != null && currentUser.ChangePassword(oldPassword, newPassword);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            catch (MembershipPasswordException)
+            {
+                return false;
+            }
+        }
+    }
 
     public interface IFormsAuthenticationService
     {
         void SignIn(string userName, bool createPersistentCookie);
         void SignOut();
     }
+
+    public class FormsAuthenticationService : IFormsAuthenticationService
+    {
+        public void SignIn(string userName, bool createPersistentCookie)
+        {
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+
+            FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
+        }
+
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
     #endregion
+
+    #region Validation
+
+    public static class AccountValidation
+    {
+        public static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+    }
+
+    #endregion
+
 
 
 }
